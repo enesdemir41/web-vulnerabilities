0337f157d2d20181b7d88deecdb879cc78275034
enesdemir41 <49167863+enesdemir41@users.noreply.github.com>
2024-09-10 20:29:50 +0300

    Update serialize.php

--- deserializationzafiyet/serialize.php
+++ deserializationzafiyet/serialize.php
@@ -23,7 +23,7 @@ class user {
 
 $str = 'O:4:"user":2:{s:9:"firstname";s:4:"enes";s:8:"lastname";s:5:"demir";}';
 
- $user = unserialize($str); // aslında burayı $user = new user() olarak düşünebilirsin direkt str deserliaze edilerek obje olustu
- echo $user ; // strinten obje olusmus oldu BU KADAR 
+ $user = unserialize($str); 
+ echo $user ; 
 
 
